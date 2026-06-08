13eb27b85e55d2e0b614d275f9df770838de0452
austincampb <acampb@unc.edu>
2026-06-08 11:50:02 -0400

    add signin component and firebase utils

---
+++ austins-bookstore/src/utils/firebase/firebase.utils.js
@@ -0,0 +1,57 @@
+import { initializeApp } from 'firebase/app';
+import { 
+  getAuth, 
+  signInWithRedirect, 
+  signInWithPopup, 
+  GoogleAuthProvider 
+} from 'firebase/auth';
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDmbUY_jbBTIgxuuPkuiWacYK1hopArOKQ",
+  authDomain: "austins-bookstore-db.firebaseapp.com",
+  projectId: "austins-bookstore-db",
+  storageBucket: "austins-bookstore-db.firebasestorage.app",
+  messagingSenderId: "102592251428",
+  appId: "1:102592251428:web:e5f2a025684c17e9e5fab1"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+  
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    };
+  };
+
+  return userDocRef;
+}
